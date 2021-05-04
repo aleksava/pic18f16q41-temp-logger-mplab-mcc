815e2e5a9efae7d947337a0b8006d1f6ae774598
glen.nilsen@microchip.com <glen.nilsen@microchip.com>
2021-05-04 09:49:40 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Q10-MCC-Melody.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Q10-MCC-Melody.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=q10-mcc-melody.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/q10-mcc-melody.tar
